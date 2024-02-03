38adcaed6b98d3ccd0a076942953477d362ecde6
Sriharish K S <harish.cyberwarrior@gmail.com>
2024-02-03 15:19:17 +0530

    spring Git test

---
+++ springapp/src/main/java/com/spring/domain/Author.java
@@ -0,0 +1,61 @@
+package com.spring.domain;
+
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> getBook;
+
+
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getGetBook() {
+        return getBook;
+    }
+
+    public void setGetBook(Set<Book> getBook) {
+        this.getBook = getBook;
+    }
+
+
+
+}
